3207d7bb46a01c30312bd278a95c1898995a6dc1
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-01-01 00:26:05 +0900

    Update Beautiful-Days-at-the-Movies.cpp

--- Beautiful-Days-at-the-Movies.cpp
+++ Beautiful-Days-at-the-Movies.cpp
@@ -1,3 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
 int beautifulDays(int i, int j, int k)
 {
     int count = 0;
